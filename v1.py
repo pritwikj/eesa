45fe416809db9735fa0db2910cc5fd25a4b3ed77
Pritwik <pritwkjeeva@eesa.ai>
2023-10-16 20:12:57 -0700

    openai api key

--- v1.py
+++ v1.py
@@ -1,7 +1,7 @@
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
 import os
 
-os.environ['OPENAI_API_KEY'] = 'sk-test'
+os.environ['OPENAI_API_KEY'] = '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'
 
 
 print("BOOTY FACE")
