d86f0815bea922c2da99a3a7a781f8625d2ee9b2
Pritwik <pritwkjeeva@eesa.ai>
2023-11-17 12:42:49 -0800

    test commit

--- class_test.py
+++ class_test.py
@@ -3,6 +3,7 @@ import sqlite3
 from datetime import datetime
 import time
 from task_prioritization import *
+import threading
 
 
 # Function to initialize the database
@@ -74,7 +75,6 @@ def get_master_task_list(client_name, conn):
 
 
 
-
 #@st.cache_data(show_spinner=False)
 def contract_helper(client_name, conn):
     master_task_list = ''
@@ -114,8 +114,53 @@ def pull_emails(history_id, client_name, master_task_list, conn):
     else:
         pass
 
-# Function to run the main application
-def main():    
+
+
+# # Function to run the main application
+# def main():    
+#     conn = sqlite3.connect('task_database.db')
+
+#     initialize_database(conn)
+
+#     # Get the existing clients from the database
+#     existing_clients = set()
+#     c = conn.cursor()
+#     c.execute('SELECT DISTINCT client_name FROM tasks')
+#     result = c.fetchall()
+#     existing_clients.update(client[0] for client in result)
+#     #conn.close()
+
+#     # Streamlit interface for adding a new client
+#     new_client = st.text_input("Add a new client:")
+#     if new_client not in existing_clients:
+#         st.write(f"Added new client: {new_client}")
+#         existing_clients.add(new_client)
+
+#     clients = st.selectbox("Select a client", list(existing_clients), index=0)
+
+#     if clients == "":
+#         st.write("Start adding your clients in the settings page!")
+#     else:
+#         retrieved_task_list = get_master_task_list(clients, conn)
+#         if retrieved_task_list is None:
+#             contract_helper(clients, conn)
+
+#         placeholder = st.empty()
+#         watch_response = getwatchResponse()
+#         history_id = watch_response['historyId']
+#         while True:
+#             pull_emails(history_id, clients, retrieved_task_list, conn)
+
+#             # Retrieve and display the master task list from the database
+#             retrieved_task_list = get_master_task_list(clients, conn)
+#             placeholder.empty()
+#             placeholder.write(retrieved_task_list)
+#             watch_response = getwatchResponse()
+#             history_id = watch_response['historyId']
+#             time.sleep(10)
+
+
+def main():
     conn = sqlite3.connect('task_database.db')
 
     initialize_database(conn)
@@ -126,7 +171,7 @@ def main():
     c.execute('SELECT DISTINCT client_name FROM tasks')
     result = c.fetchall()
     existing_clients.update(client[0] for client in result)
-    #conn.close()
+    # conn.close()
 
     # Streamlit interface for adding a new client
     new_client = st.text_input("Add a new client:")
@@ -137,18 +182,26 @@ def main():
     clients = st.selectbox("Select a client", list(existing_clients), index=0)
 
     if clients == "":
-        st.write("Start adding your clients in the settings page!")
+        st.write("Start adding your clients on the settings page!")
     else:
         retrieved_task_list = get_master_task_list(clients, conn)
         if retrieved_task_list is None:
             contract_helper(clients, conn)
 
         placeholder = st.empty()
-        watch_response = getwatchResponse()
-        history_id = watch_response['historyId']
-        while True:
-            pull_emails(history_id, clients, retrieved_task_list, conn)
+        
+        # Function to run in a separate thread for background updates
+        def update_task_list_background():
+            while True:
+                history_id = getwatchResponse()['historyId']
+                pull_emails(history_id, clients, retrieved_task_list, conn)
+                time.sleep(10)
 
+        # Start a separate thread for background updates
+        update_thread = threading.Thread(target=update_task_list_background, daemon=True)
+        update_thread.start()
+
+        while True:
             # Retrieve and display the master task list from the database
             retrieved_task_list = get_master_task_list(clients, conn)
             placeholder.empty()
@@ -156,7 +209,6 @@ def main():
             watch_response = getwatchResponse()
             history_id = watch_response['historyId']
             time.sleep(10)
-
 
 
 if __name__ == '__main__':
