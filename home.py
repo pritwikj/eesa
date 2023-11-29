17b8ce3acedf72327a9d67f72c3ceb166278a3c4
Pritwik <pritwkjeeva@eesa.ai>
2023-11-29 11:33:14 -0800

    Able to process different clients' emails

--- home.py
+++ home.py
@@ -1,176 +1,250 @@
 import streamlit as st
+from streamlit.runtime.scriptrunner import add_script_run_ctx
+import sqlite3
+from datetime import datetime
+import time
 from task_prioritization import *
+import threading
 
-class Client:
-    def __init__(self, name, email_address):
-        self.name = name
-        self.email_address = email_address
-        self.data_updated = False
 
-    def email_helper(_self, history_id, client_email):
-        email_data = getlatestEmail(history_id, client_email)
-        # Generate the updated value based on the current value
-        watch_response = getwatchResponse()
-        history_id = watch_response['historyId']
+# Function to initialize the database
+def initialize_database():
+    conn = sqlite3.connect('task_database.db')
+    c = conn.cursor()
 
-        if email_data != None:
-            st.write("Processing pt.2...")
-            email_tasks = clean_list(email_extract(email_data))
-            st.info(email_tasks)
-            return email_tasks
-   
+    # Create the tasks table if not exists
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            client_name TEXT,
+            client_email TEXT,
+            task_list TEXT,
+            timestamp DATETIME
+        )
+    ''')
 
-    #@st.cache_data(show_spinner=False)
-    def contract_helper(_self):
-        master_task_list = ''
-        placeholder = st.empty()
-        with st.spinner("Processing contract... Do not exit page"):
-            preclean = prioritize_tasks(contract_extract())
-            master_task_list += clean_list(preclean)
-        placeholder.info("Contract processed!")
-        time.sleep(1)
-        placeholder.empty()
-        return master_task_list
+    conn.commit()
+    conn.close()
     
 
-    @st.cache_data(ttl=30)
-    def pull_test(_self):
-        varchar = 0
-        while True:
-            varchar += 1
-            if varchar == 5:
-                _self.data_updated = True
-                _self.data_test = "Data"
-                break
-                
-            time.sleep(1)
+# Function to add or update a master task list for a client in the database
+def add_or_update_master_task_list(client_name, client_email, task_list):
+    conn = sqlite3.connect('task_database.db')
+    c = conn.cursor()
 
-    def streamlit_test(_self):
+    # Check if the client exists in the database
+    c.execute('SELECT * FROM tasks WHERE client_name = ?', (client_name,))
+    existing_client = c.fetchone()
+
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    if existing_client:
+        # Update the existing client's task list and timestamp
+        c.execute('''
+            UPDATE tasks
+            SET task_list = ?,
+                timestamp = ?
+            WHERE client_name = ?
+        ''', (task_list, timestamp, client_name))
+    else:
+        # Insert the new client and their task list
+        c.execute('''
+            INSERT INTO tasks (client_name, client_email, task_list, timestamp)
+            VALUES (?, ?, ?, ?)
+        ''', (client_name, client_email, task_list, timestamp))
+
+    conn.commit()
+    conn.close()
+
+
+# Function to get the master task list for a client from the database
+def get_master_task_list(client_name):
+    conn = sqlite3.connect('task_database.db')
+    c = conn.cursor()
+
+    # Select the latest task list for the given client
+    c.execute('''
+        SELECT task_list
+        FROM tasks
+        WHERE client_name = ?
+        ORDER BY timestamp DESC
+        LIMIT 1
+    ''', (client_name,))
+    
+    result = c.fetchone()
+
+    conn.close()
+
+    return result[0] if result else None
+
+
+def contract_helper(client_name, client_email):
+    master_task_list = ''
+    placeholder_c = st.empty()
+    with st.spinner(f"Processing contract for {client_name}... Do not exit page"):
+        preclean = prioritize_tasks(contract_extract())
+        master_task_list += clean_list(preclean)
+    placeholder_c.info("Contract processed!")
+    time.sleep(2)
+    placeholder_c.empty()
+
+    # Add the master task list to the database
+    add_or_update_master_task_list(client_name, client_email, master_task_list)
+    
+
+def email_helper(history_id, client_email):
+    email_data = getlatestEmail(history_id, client_email)
+    if email_data is not None:
+        email_tasks = clean_list(email_extract(email_data))
+        return email_tasks
+    else:
+        return None
+
+def pull_emails(history_id, client_name, client_email, master_task_list):
+    # email_tasks = email_helper(history_id, client_email)
+
+    # if email_tasks is None:
+    #     master_task_list2 = master_task_list
+    # else:
+    #     master_task_list2 = clean_list(update_master_tasks(master_task_list, email_tasks))
+
+    # if master_task_list2 != master_task_list:
+    #     add_or_update_master_task_list(client_name, client_email, master_task_list2)
+    #     master_task_list = master_task_list2
+    # else:
+    #     pass
+
+    email_tasks = email_helper(history_id, client_email)
+
+    if email_tasks is not None:
+        master_task_list2 = clean_list(update_master_tasks(master_task_list, email_tasks)) 
+        add_or_update_master_task_list(client_name, client_email, master_task_list2)
+    else:
+        pass
+
+
+def pull_calls(client_name, client_email, master_task_list):
+    call_tasks = call_extract()
+    master_task_list2 = clean_list(update_master_tasks(master_task_list, call_tasks)) 
+    add_or_update_master_task_list(client_name, client_email, master_task_list2)
+
+
+
+def main():
+    conn = sqlite3.connect('task_database.db')
+    initialize_database()
+
+    # Get the existing clients from the database
+    existing_clients = set()
+    c = conn.cursor()
+    c.execute('SELECT DISTINCT client_name FROM tasks')
+    result = c.fetchall()
+    existing_clients.update(client[0] for client in result)
+    
+    # Get the existing email addresses from the database
+    existing_emails = set()
+    c.execute('SELECT DISTINCT client_email FROM tasks')
+    result = c.fetchall()
+    existing_emails.update(email[0] for email in result)
+    
+
+    # Streamlit interface for adding a new client
+    if 'new_client' not in st.session_state:
+        st.session_state.new_client = ''
+
+    def submit_client():
+        st.session_state.new_client = st.session_state.c_widget
+        st.session_state.c_widget = ''
+    
+    if 'new_email' not in st.session_state:
+        st.session_state.new_email = ''
+
+    def submit_email():
+        st.session_state.new_email = st.session_state.e_widget
+        st.session_state.e_widget = ''
+
+    st.sidebar.text_input("Add a new client:", key='c_widget', on_change=submit_client)
+    new_client = st.session_state.new_client
+    
+    st.sidebar.text_input("Add the client's email:", key='e_widget', on_change=submit_email)
+    client_email = st.session_state.new_email
+
+
+
+
+    if new_client != "" and new_client not in existing_clients and client_email != "" and client_email not in existing_emails:
+        placeholder_add = st.empty()
+        placeholder_add.info(f"Added new client: {new_client}")
+        time.sleep(2)
+        placeholder_add.empty()
+        existing_clients.add(new_client)
+        existing_emails.add(client_email)
+
+        # Start a background thread for the new client
+        background_thread = threading.Thread(target=background_task, args=(new_client, client_email,))
+        add_script_run_ctx(background_thread)
+        background_thread.start()
+
+    clients = st.sidebar.selectbox("Select a client", list(existing_clients), index=0)
+
+    if clients == None:
+        st.write("Start adding your clients on the left side!")
+    else:
+        if 'title' in st.session_state:
+            st.session_state['title'].empty()
+        else:
+            st.session_state['title'] = st.empty()
+
+        st.session_state['title'].title("Client: " + clients)
+
+        if 'placeholder' in st.session_state:
+            st.session_state['placeholder'].empty()
+        else:
+            st.session_state['placeholder'] = st.empty()
+
+        st.session_state['placeholder'].empty()
+        
+        retrieved_task_list = get_master_task_list(clients)
+        if retrieved_task_list is None:
+            #contract_helper(clients, client_email)
+            #time.sleep(5)
+            add_or_update_master_task_list(clients, client_email, "No tasks yet")
+        
         while True:
-            _self.pull_test()
-            st.write(_self.data_updated)
-            if _self.data_updated:
-                st.info(_self.data_test)
-            
+            retrieved_task_list = get_master_task_list(clients)
+
+            st.session_state['placeholder'].empty()
+            st.session_state['placeholder'].write(retrieved_task_list)
             time.sleep(5)
 
 
-    #@st.cache_data(ttl=30)
-    def pull_emails(_self, history_id, master_task_list):
-        
-        client_email = _self.email_address
-        email_tasks = _self.email_helper(history_id, client_email)
-        placeholder = st.empty()
-
-        
-        # Generate the updated value based on the current value
-        watch_response = getwatchResponse()
-        history_id = watch_response['historyId']
-        
-        if email_tasks == None:
-            master_task_list2 = master_task_list
-        else:
-            st.write("Processing pt.3...")
-            master_task_list2 = clean_list(update_master_tasks(master_task_list, email_tasks))
-
-        if master_task_list2 != master_task_list:
-            st.session_state['master_tasks'][_self.name] = master_task_list2
-            placeholder.empty()
-            placeholder.info(st.session_state['master_tasks'][_self.name])
-
-            master_task_list = master_task_list2
-        else:
-            pass
 
 
-    def streamlit_test3(_self):
-        # st.write(_self.name)
-        # st.write(_self.email_address)
+# Background task to update the client's task list
+def background_task(client_name, client_email):
+    watch_response = getwatchResponse()
+    history_id = watch_response['historyId']
+    #n = 0
+    while True:
+        retrieved_task_list = get_master_task_list(client_name)
 
-        client_email = _self.email_address
+        if retrieved_task_list is not None:
+            #n+=1
+            #add_or_update_master_task_list(client_name, client_email, n)
 
-        if st.session_state['master_tasks'][_self.name] == 'No tasks yet':
-            master_task_list = _self.contract_helper()
-            st.session_state['master_tasks'][_self.name] = master_task_list
-        else:
-            master_task_list = st.session_state['master_tasks'][_self.name]
+            pull_emails(history_id, client_name, client_email, retrieved_task_list)
+            # retrieved_task_list2 = get_master_task_list(client_name)
+            # if n != 1:
+            #     pull_calls(client_name, client_email, retrieved_task_list2)
+            #n = 1
 
-        watch_response = getwatchResponse()
-        history_id = watch_response['historyId'] #Initializing history_id
-        
-
-        while True:
-            _self.pull_emails(history_id, master_task_list)
-            #st.info(st.session_state['master_tasks'][_self.name])
-
-            # Wait for 10 seconds
-            time.sleep(1)
-
-
-
-    def streamlit_test2(_self):
-        # st.write(_self.name)
-        # st.write(_self.email_address)
-
-        client_email = _self.email_address
-
-        if st.session_state['master_tasks'][_self.name] == 'No tasks yet':
-            master_task_list = _self.contract_helper()
-            st.session_state['master_tasks'][_self.name] = master_task_list
-        else:
-            master_task_list = st.session_state['master_tasks'][_self.name]
-
-        watch_response = getwatchResponse()
-        history_id = watch_response['historyId'] #Initializing history_id
-        
-
-        while True:
-            email_tasks = _self.email_helper(history_id, client_email)
-            
-            # Generate the updated value based on the current value
             watch_response = getwatchResponse()
             history_id = watch_response['historyId']
-            
-            if email_tasks == None:
-                master_task_list2 = master_task_list
-            else:
-                st.write("Processing pt.3...")
-                master_task_list2 = clean_list(update_master_tasks(master_task_list, email_tasks))
 
-            if master_task_list2 != master_task_list:
-                st.session_state['master_tasks'][_self.name] = master_task_list2
-                st.info(st.session_state['master_tasks'][_self.name])
-
-                master_task_list = master_task_list2
-            else:
-                pass
-
-        
-            # Wait for 10 seconds
-            time.sleep(10)
+        time.sleep(5)
 
 
 
 
-
-st.title("Eesa")
-if 'clients' not in st.session_state:
-    st.session_state['dropdown'] = ["Overview"]
-    st.session_state['clients'] = {}
-    st.session_state['master_tasks'] = {}
-    
-
-client_select = st.sidebar.selectbox("Select a client to display tasks for", st.session_state['dropdown'])
-
-
-if client_select != "Overview":
-    if st.session_state['master_tasks'][client_select] == '':
-        st.session_state['master_tasks'][client_select] = "No tasks yet"
-
-    st.info(st.session_state['master_tasks'][client_select])
-
-    st.session_state['clients'][client_select].streamlit_test3()
-
-else:
-    st.info("Start adding your clients in the settings page!")
-
+if __name__ == '__main__':
+    main()
--- v1.py
+++ v1.py
@@ -1,151 +1,176 @@
 import streamlit as st
-import sqlite3
-from datetime import datetime
-import time
 from task_prioritization import *
 
+class Client:
+    def __init__(self, name, email_address):
+        self.name = name
+        self.email_address = email_address
+        self.data_updated = False
 
-# Function to initialize the database
-def initialize_database():
-    conn = sqlite3.connect('task_database.db')
-    c = conn.cursor()
-
-    # Create the tasks table if not exists
-    c.execute('''
-        CREATE TABLE IF NOT EXISTS tasks (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            client_name TEXT,
-            task_list TEXT,
-            timestamp DATETIME
-        )
-    ''')
-
-    conn.commit()
-    conn.close()
-
-# Function to add a master task list for a client to the database
-def add_master_task_list(client_name, task_list):
-    conn = sqlite3.connect('task_database.db')
-    c = conn.cursor()
-
-    # Insert the task list into the database with the current timestamp
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    c.execute('INSERT INTO tasks (client_name, task_list, timestamp) VALUES (?, ?, ?)', (client_name, task_list, timestamp))
-
-    conn.commit()
-    conn.close()
-
-# Function to update the master task list for a client in the database
-def update_master_task_list(client_name, new_task_list):
-    conn = sqlite3.connect('task_database.db')
-    c = conn.cursor()
-
-    # Update the task_list for the given client
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    c.execute('''
-        UPDATE tasks
-        SET task_list = ?,
-            timestamp = ?
-        WHERE client_name = ?
-    ''', (new_task_list, timestamp, client_name))
-
-    conn.commit()
-    conn.close()
-
-# Function to get the master task list for a client from the database
-def get_master_task_list(client_name):
-    conn = sqlite3.connect('task_database.db')
-    c = conn.cursor()
-
-    # Select the latest task list for the given client
-    c.execute('''
-        SELECT task_list
-        FROM tasks
-        WHERE client_name = ?
-        ORDER BY timestamp DESC
-        LIMIT 1
-    ''', (client_name,))
-    
-    result = c.fetchone()
-
-    conn.close()
-
-    return result[0] if result else None
-
-@st.cache_data(show_spinner=False)
-def contract_helper():
-    client_name = "Test 1"
-    master_task_list = ''
-    placeholder = st.empty()
-    with st.spinner("Processing contract... Do not exit page"):
-        preclean = prioritize_tasks(contract_extract())
-        master_task_list += clean_list(preclean)
-    placeholder.info("Contract processed!")
-    time.sleep(1)
-    placeholder.empty()
-
-    # Add the master task list to the database
-    add_master_task_list(client_name, master_task_list)
-
-    #return master_task_list
-
-def email_helper(history_id, client_email):
-    email_data = getlatestEmail(history_id, client_email)
-    # Generate the updated value based on the current value
-    # watch_response = getwatchResponse()
-    # history_id = watch_response['historyId']
-
-    if email_data != None:
-        st.write("Processing pt.2...")
-        email_tasks = clean_list(email_extract(email_data))
-        return email_tasks
-    
-def pull_emails(history_id, master_task_list):
-    email_address = "pritwik@skoruz.com"
-    client_name = "Test 1"
-    client_email = email_address
-    email_tasks = email_helper(history_id, client_email)
-
-    
-    # # Generate the updated value based on the current value
-    # watch_response = getwatchResponse()
-    # history_id = watch_response['historyId']
-    
-    if email_tasks == None:
-        master_task_list2 = master_task_list
-    else:
-        st.write("Processing pt.3...")
-        master_task_list2 = clean_list(update_master_tasks(master_task_list, email_tasks))
-
-    if master_task_list2 != master_task_list:
-        update_master_task_list(client_name, master_task_list2)
-        master_task_list = master_task_list2
-    else:
-        pass
-
-# Function to run the main application
-def main():
-    initialize_database()    
-
-    retrieved_task_list = get_master_task_list("Test 1")
-    if retrieved_task_list is None:
-        contract_helper()
-    st.write(retrieved_task_list)
-
-
-    placeholder = st.empty()
-    watch_response = getwatchResponse()
-    history_id = watch_response['historyId']
-    while True:
-        pull_emails(history_id, retrieved_task_list)
-
-        # Retrieve and display the master task list from the database
-        retrieved_task_list = get_master_task_list("Test 1")
-        placeholder.empty()
-        placeholder.write(retrieved_task_list)
+    def email_helper(_self, history_id, client_email):
+        email_data = getlatestEmail(history_id, client_email)
+        # Generate the updated value based on the current value
         watch_response = getwatchResponse()
         history_id = watch_response['historyId']
-        time.sleep(10)
 
-if __name__ == '__main__':    
-    main()
+        if email_data != None:
+            st.write("Processing pt.2...")
+            email_tasks = clean_list(email_extract(email_data))
+            st.info(email_tasks)
+            return email_tasks
+   
+
+    #@st.cache_data(show_spinner=False)
+    def contract_helper(_self):
+        master_task_list = ''
+        placeholder = st.empty()
+        with st.spinner("Processing contract... Do not exit page"):
+            preclean = prioritize_tasks(contract_extract())
+            master_task_list += clean_list(preclean)
+        placeholder.info("Contract processed!")
+        time.sleep(1)
+        placeholder.empty()
+        return master_task_list
+    
+
+    @st.cache_data(ttl=30)
+    def pull_test(_self):
+        varchar = 0
+        while True:
+            varchar += 1
+            if varchar == 5:
+                _self.data_updated = True
+                _self.data_test = "Data"
+                break
+                
+            time.sleep(1)
+
+    def streamlit_test(_self):
+        while True:
+            _self.pull_test()
+            st.write(_self.data_updated)
+            if _self.data_updated:
+                st.info(_self.data_test)
+            
+            time.sleep(5)
+
+
+    #@st.cache_data(ttl=30)
+    def pull_emails(_self, history_id, master_task_list):
+        
+        client_email = _self.email_address
+        email_tasks = _self.email_helper(history_id, client_email)
+        placeholder = st.empty()
+
+        
+        # Generate the updated value based on the current value
+        watch_response = getwatchResponse()
+        history_id = watch_response['historyId']
+        
+        if email_tasks == None:
+            master_task_list2 = master_task_list
+        else:
+            st.write("Processing pt.3...")
+            master_task_list2 = clean_list(update_master_tasks(master_task_list, email_tasks))
+
+        if master_task_list2 != master_task_list:
+            st.session_state['master_tasks'][_self.name] = master_task_list2
+            placeholder.empty()
+            placeholder.info(st.session_state['master_tasks'][_self.name])
+
+            master_task_list = master_task_list2
+        else:
+            pass
+
+
+    def streamlit_test3(_self):
+        # st.write(_self.name)
+        # st.write(_self.email_address)
+
+        client_email = _self.email_address
+
+        if st.session_state['master_tasks'][_self.name] == 'No tasks yet':
+            master_task_list = _self.contract_helper()
+            st.session_state['master_tasks'][_self.name] = master_task_list
+        else:
+            master_task_list = st.session_state['master_tasks'][_self.name]
+
+        watch_response = getwatchResponse()
+        history_id = watch_response['historyId'] #Initializing history_id
+        
+
+        while True:
+            _self.pull_emails(history_id, master_task_list)
+            #st.info(st.session_state['master_tasks'][_self.name])
+
+            # Wait for 10 seconds
+            time.sleep(1)
+
+
+
+    def streamlit_test2(_self):
+        # st.write(_self.name)
+        # st.write(_self.email_address)
+
+        client_email = _self.email_address
+
+        if st.session_state['master_tasks'][_self.name] == 'No tasks yet':
+            master_task_list = _self.contract_helper()
+            st.session_state['master_tasks'][_self.name] = master_task_list
+        else:
+            master_task_list = st.session_state['master_tasks'][_self.name]
+
+        watch_response = getwatchResponse()
+        history_id = watch_response['historyId'] #Initializing history_id
+        
+
+        while True:
+            email_tasks = _self.email_helper(history_id, client_email)
+            
+            # Generate the updated value based on the current value
+            watch_response = getwatchResponse()
+            history_id = watch_response['historyId']
+            
+            if email_tasks == None:
+                master_task_list2 = master_task_list
+            else:
+                st.write("Processing pt.3...")
+                master_task_list2 = clean_list(update_master_tasks(master_task_list, email_tasks))
+
+            if master_task_list2 != master_task_list:
+                st.session_state['master_tasks'][_self.name] = master_task_list2
+                st.info(st.session_state['master_tasks'][_self.name])
+
+                master_task_list = master_task_list2
+            else:
+                pass
+
+        
+            # Wait for 10 seconds
+            time.sleep(10)
+
+
+
+
+
+st.title("Eesa")
+if 'clients' not in st.session_state:
+    st.session_state['dropdown'] = ["Overview"]
+    st.session_state['clients'] = {}
+    st.session_state['master_tasks'] = {}
+    
+
+client_select = st.sidebar.selectbox("Select a client to display tasks for", st.session_state['dropdown'])
+
+
+if client_select != "Overview":
+    if st.session_state['master_tasks'][client_select] == '':
+        st.session_state['master_tasks'][client_select] = "No tasks yet"
+
+    st.info(st.session_state['master_tasks'][client_select])
+
+    st.session_state['clients'][client_select].streamlit_test3()
+
+else:
+    st.info("Start adding your clients in the settings page!")
+
